2fc654ffb3ca48cf8ecb06550bb074e4381079d2
tomek <tomeklorenz@gmail.com>
2022-01-25 19:54:44 +0100

    Working on user performance

--- Sudoku_v2.py
+++ Sudoku_v2.py
@@ -9,6 +9,11 @@ pygame.init()
 pygame.font.init()
 pygame.display.set_caption('SUDOKU')
 
+BLACK = [0, 0, 0]
+ORANGE = [255, 129, 0]
+WHITE = [255, 255, 255]
+DARK_GRAY = [180, 180, 180]
+
 class Square(ABC):
     def __init__(self, width, pos_x, pos_y):
         self.width = width
@@ -22,9 +27,9 @@ class Square(ABC):
 class Grid(Square):
 
     OFFSET = 25
-    BLACK = [0, 0, 0]
-    ORANGE = [255, 129, 0]
-    WHITE = [255, 255, 255]
+    # BLACK = [0, 0, 0]
+    # ORANGE = [255, 129, 0]
+    # WHITE = [255, 255, 255]
 
     def __init__(self, width, pos_x, pos_y, window):
         super().__init__(width, pos_x, pos_y)
@@ -32,7 +37,7 @@ class Grid(Square):
         self.window = window
 
     def draw_background(self):
-        pygame.draw.rect(self.window, self.WHITE, (self.OFFSET, self.OFFSET, self.width - 50, self.width - 50))
+        pygame.draw.rect(self.window, WHITE, (self.OFFSET, self.OFFSET, self.width - 50, self.width - 50))
 
     def draw(self):
         sizeBtwn = (self.width - 50) // self.rows
@@ -41,19 +46,19 @@ class Grid(Square):
 
         for l in range(self.rows + 1):		
             if l == 3 or l == 6:
-                pygame.draw.line(self.window, self.ORANGE, (x, self.OFFSET), (x, self.width - self.OFFSET), 3)
-                pygame.draw.line(self.window, self.ORANGE, (self.OFFSET, y), (self.width - self.OFFSET, y), 3)
+                pygame.draw.line(self.window, ORANGE, (x, self.OFFSET), (x, self.width - self.OFFSET), 3)
+                pygame.draw.line(self.window, ORANGE, (self.OFFSET, y), (self.width - self.OFFSET, y), 3)
             elif l != 0 or l != 9:
-                pygame.draw.line(self.window, self.ORANGE, (x, self.OFFSET), (x, self.width - self.OFFSET))
-                pygame.draw.line(self.window, self.ORANGE, (self.OFFSET, y), (self.width - self.OFFSET, y))
+                pygame.draw.line(self.window, ORANGE, (x, self.OFFSET), (x, self.width - self.OFFSET))
+                pygame.draw.line(self.window, ORANGE, (self.OFFSET, y), (self.width - self.OFFSET, y))
     
             x += sizeBtwn
             y += sizeBtwn
 
-        pygame.draw.line(self.window, self.BLACK, (self.OFFSET, self.OFFSET), (self.OFFSET, self.width - self.OFFSET), 3)
-        pygame.draw.line(self.window, self.BLACK, (self.OFFSET, self.OFFSET), (self.width - self.OFFSET, self.OFFSET), 3)
-        pygame.draw.line(self.window, self.BLACK, (19 * self.OFFSET, self.OFFSET), (19*self.OFFSET, self.width - self.OFFSET), 3)
-        pygame.draw.line(self.window, self.BLACK, (self.OFFSET, 19 * self.OFFSET), (self.width - self.OFFSET, 19 * self.OFFSET), 3)
+        pygame.draw.line(self.window, BLACK, (self.OFFSET, self.OFFSET), (self.OFFSET, self.width - self.OFFSET), 3)
+        pygame.draw.line(self.window, BLACK, (self.OFFSET, self.OFFSET), (self.width - self.OFFSET, self.OFFSET), 3)
+        pygame.draw.line(self.window, BLACK, (19 * self.OFFSET, self.OFFSET), (19*self.OFFSET, self.width - self.OFFSET), 3)
+        pygame.draw.line(self.window, BLACK, (self.OFFSET, 19 * self.OFFSET), (self.width - self.OFFSET, 19 * self.OFFSET), 3)
 
 
 # Each field has got only one position
@@ -90,20 +95,20 @@ class Info(Grid):
         self.game_state = False
 
     def draw_background(self):
-        pygame.draw.rect(self.window, self.WHITE, (25, 525, self.width - 50, 50))
+        pygame.draw.rect(self.window, WHITE, (25, 525, self.width - 50, 50))
 
     def draw(self):
 
         # Inner Vertical Lines
         for i in range(1, 9):
-            pygame.draw.line(self.window, self.ORANGE, (25 + i * 50, 525), (25 + i * 50, 575))
+            pygame.draw.line(self.window, ORANGE, (25 + i * 50, 525), (25 + i * 50, 575))
 
         # Horizontal lines
-        pygame.draw.line(self.window, self.BLACK, (25, 525), (475, 525), 3)
-        pygame.draw.line(self.window, self.BLACK, (25, 575), (475, 575), 3)
+        pygame.draw.line(self.window, BLACK, (25, 525), (475, 525), 3)
+        pygame.draw.line(self.window, BLACK, (25, 575), (475, 575), 3)
         # Outer Vertical lines
-        pygame.draw.line(self.window, self.BLACK, (25, 525), (25, 575), 3)
-        pygame.draw.line(self.window, self.BLACK, (475, 525), (475, 575), 3)
+        pygame.draw.line(self.window, BLACK, (25, 525), (25, 575), 3)
+        pygame.draw.line(self.window, BLACK, (475, 525), (475, 575), 3)
 
         # Game timer
         current_time = datetime.now()
@@ -122,7 +127,7 @@ class Info(Grid):
             output = f"0{minutes}:00"
         elif seconds == 60 and minutes >= 10:
             output = f"{minutes}:00"
-        text = self.font.render(output, True, self.BLACK)      
+        text = self.font.render(output, True, BLACK)      
         textRect = text.get_rect()
         textRect.center = (125, 640)
         self.window.blit(text, textRect)
@@ -133,10 +138,10 @@ class Info(Grid):
         else:
             game_info = "PAUSE"
 
-        pygame.draw.circle(self.window, self.ORANGE, (330, 640), 25)
-        pygame.draw.circle(self.window, self.ORANGE, (420, 640), 25)
-        pygame.draw.rect(self.window, self.ORANGE, (325, 615, 100, 50))
-        text = self.font.render(game_info, True, self.WHITE)      
+        pygame.draw.circle(self.window, ORANGE, (330, 640), 25)
+        pygame.draw.circle(self.window, ORANGE, (420, 640), 25)
+        pygame.draw.rect(self.window, ORANGE, (325, 615, 100, 50))
+        text = self.font.render(game_info, True, WHITE)      
         textRect = text.get_rect()
         textRect.center = (375, 640)
         self.window.blit(text, textRect)
@@ -154,15 +159,31 @@ class InfoField(Field):
         self.available = available
         self.quantity = 9
         if not self.available:
-            self.font_color = self.ORANGE
-            self.background = self.DARK_GRAY
+            self.font_color = ORANGE
+            self.background = DARK_GRAY
         else:
-            self.font_color =  self.BLACK
-            self.background = self.WHITE
+            self.font_color =  BLACK
+            self.background = WHITE
 
     def draw(self):
         pygame.draw.rect(self.window, self.background, (self.pos_x - 25, self.pos_y - 25, self.width, self.width))
         super().draw()
+
+    def update_quantity(self, fields):
+        count = 0
+        for row in fields:
+            for field in row:
+                if field.value == self.value and self.value != 0:
+                    count += 1
+        self.quantity = 9 - count
+        if self.quantity > 0:
+            self.available = True
+            self.font_color =  BLACK
+            self.background = WHITE
+        else:
+            self.available = False
+            self.font_color = ORANGE
+            self.background = DARK_GRAY
 
 
 # Reads the current value of the field, moves around the grid
@@ -175,9 +196,22 @@ class Cursor():
         self.pos_num = pos_num
         self.window = window
         self.positions = positions
+        self.selected = False
+        self.value_to_insert = 0
 
     def draw(self):
-        pygame.draw.rect(self.window, self.DARK_GRAY, (self.positions[self.pos_num][0], self.positions[self.pos_num][1], self.width, self.width))
+        if self.selected:
+            pygame.draw.rect(self.window, DARK_GRAY, (self.positions[self.pos_num][0], self.positions[self.pos_num][1], self.width, self.width))
+
+    def check_position(self, mouse_pos, info_fields):
+        for key, value in self.positions.items():
+            cond1 = mouse_pos[0] > value[0] and mouse_pos[0] < value[0] + 50
+            cond2 = mouse_pos[1] > value[1] and mouse_pos[1] < value[1] + 50
+            if cond1 and cond2 and key < 100:
+                self.pos_num = key
+            elif cond1 and cond2 and self.selected and key > 100:
+                if info_fields[key - 101].available:
+                    self.value_to_insert = key - 100
 
     def move_cursor(self, delta):
         if delta == 1 and self.pos_num % 9 != 0:
@@ -194,7 +228,7 @@ class Game:
 
     LIGHT_BLUE = [52, 204, 255]
 
-    def __init__(self, width, height):
+    def __init__(self, width = 500, height = 700):
         self.width = width
         self.height = height
         self.window = None
@@ -204,6 +238,8 @@ class Game:
         self.cursor = None
         self.info = None
         self.positions = {}
+        self.info_positions = {}
+        self.delta = 0
         self.run()
 
     def display_setup(self):
@@ -214,14 +250,19 @@ class Game:
         self.window.fill((255, 255, 255))
         pygame.display.flip()
         start_x = start_y = 25
-        for i in range(1,82):
+        for i in range(1, 82):
             self.positions[i] = (start_x, start_y)
             if i % 9 == 0:
                 start_y += 50
                 start_x -= 400
             else:
                 start_x += 50
-
+        start_x = 25
+        start_y = 525
+        for i in range(101, 110):
+            self.positions[i] = (start_x, start_y)
+            start_x += 50
+        
     def initialize_content(self):
         self.grid = Grid(500, 50, 50, self.window)
         self.fields = []
@@ -235,10 +276,7 @@ class Game:
         self.cursor = Cursor(50, 1, self.window, self.positions)
         self.info = Info(500, 50, 25, 525, self.window)
         for i in range(self.info.cols):
-            if i % 2:
-                info_field = InfoField(50, 50 * (i + 1), 550, i + 1, self.window, True, False)
-            else:
-                info_field = InfoField(50, 50 * (i + 1), 550, i + 1, self.window, True, True)
+            info_field = InfoField(50, 50 * (i + 1), 550, i + 1, self.window, True, True)
             self.info_fields.append(info_field)
 
     def redraw_content(self):
@@ -268,7 +306,37 @@ class Game:
                 self.delta = 9
             else:
                 self.delta = 0
-        self.cursor.move_cursor(self.delta)
+        if self.cursor.selected:
+            self.cursor.move_cursor(self.delta)
+
+    def insert_value(self):
+        # self.cursor.pos_num
+        # self.cursor.value_to_insert
+        if self.cursor.value_to_insert != 0:
+            temp_row = self.cursor.pos_num / 9
+            if temp_row <= 1:
+                row = 0
+            elif 1 < temp_row <= 2:
+                row = 1
+            elif 2 < temp_row <= 3:
+                row = 2
+            elif 3 < temp_row <= 4:
+                row = 3
+            elif 4 < temp_row <= 5:
+                row = 4
+            elif 5 < temp_row <= 6:
+                row = 5
+            elif 6 < temp_row <= 7:
+                row = 6
+            elif 7 < temp_row <= 8:
+                row = 7
+            elif 8 < temp_row <= 9:
+                row = 8
+            temp_pos = self.cursor.pos_num % 9
+            if temp_pos == 0:
+                self.fields[8][row].value = self.cursor.value_to_insert
+            else:
+                self.fields[temp_pos - 1][row].value = self.cursor.value_to_insert
 
     def run(self):
         self.display_setup()
@@ -282,11 +350,27 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if pygame.mouse.get_pressed(3)[0]:
+                        self.cursor.value_to_insert = 0
+                        self.cursor.selected = True
+                        pygame.display.update()
+                        self.cursor.check_position(pygame.mouse.get_pos(), self.info_fields)
+                        self.insert_value()
+                        if self.cursor.value_to_insert != 0:
+                            for info_field in self.info_fields:
+                                info_field.update_quantity(self.fields)
+                                print(f"Number: {info_field.value} | Quantity: {info_field.quantity} | Available: {info_field.available}")
+                    elif pygame.mouse.get_pressed(3)[2]:
+                        self.cursor.selected = False
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_RETURN:
+                            pass
                 if running == False:
                     pygame.quit()
-                self.user_actions()
+                # self.user_actions()
 
 if __name__ == "__main__":
-    g = Game(500, 700)
+    g = Game()
 
 
